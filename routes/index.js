9660b306f74c59f193a0b8ee28eab8e2d7b36e63
Timothy Brown <tabrownies@gmail.com>
2020-11-24 08:19:04 -0700

    Johnny-five commit two

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var cors = require('cors');
+const Raspi = require('raspi-io').RaspiIO;
+const five = require('johnny-five');
+const board = new five.board({
+  io: new Raspi()
+});
+board.on("ready", () => {
+  const led = new five.Pin(7);
+  led('high');
+})
 router.use(cors());
 /* GET home page. */
 router.get('/armenian', function(req, res, next) {
