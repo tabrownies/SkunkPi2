1d2c876c6472b688cf70c7f09435ebd3ad33a3db
Timothy Brown <tabrownies@gmail.com>
2020-11-24 14:45:21 -0700

    GPIO try 2 with onoff

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Gpio = require('onoff').Gpio;
 var cors = require('cors');
-const Raspi = require('raspi-io').RaspiIO;
-const five = require('johnny-five');
-const board = new five.board({
-  io: new Raspi()
-});
-board.on("ready", () => {
-  const led = new five.Pin(7);
-  led('high');
-})
 router.use(cors());
 /* GET home page. */
+var LED = new Gpio(4, 'out');
 router.get('/armenian', function(req, res, next) {
   res.send(true);
+});
+router.post('/on', function(res,req){
+  LED.writeSync(1);
+  res.send('on');
+});
+router.post('/off', function(res,req){
+  LED.writeSync(0);
+  res.send('off');
 });
 
 module.exports = router;
